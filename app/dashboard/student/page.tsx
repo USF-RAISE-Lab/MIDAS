aa12b12d630002792cacaabd806a393d95134bc3
Gabriel Hooks <gabriel.i.hooks@gmail.com>
2024-09-23 14:58:45 -0400

    removed old student dash and began adding new components

--- app/dashboard/student/page.tsx
+++ app/dashboard/student/page.tsx
@@ -14,6 +14,10 @@ import { Button } from '@/app/ui/button';
 import { ArrowDownTrayIcon } from '@heroicons/react/24/outline';
 import { postData } from '@/app/lib/helpers';
 import { writeFile } from 'xlsx';
+import useMidasStore, { SchoolData } from '@/hooks/useSchoolData';
+import { calculateModeConfidence, calculateOccurancePercentages, calculateRiskByDemographic, calculateRiskPercentages, calculateTestRiskPercentages } from '@/action/calculateRiskStatistics';
+import { RiskCard } from '@/app/ui/dashboard/risk-card';
+import { MidasRiskScoreTooltip } from '@/app/ui/textblocks/tooltips';
 
 interface SearchProps {
   searchParams: {
@@ -22,206 +26,145 @@ interface SearchProps {
 }
 
 export default function Page({ searchParams }: SearchProps) {
-  const schoolLevel = useSchoolLevel();
-  const stateStudent = useSearchContext('student');
-  const setSelectedStudent = stateStudent.set;
-  const selectedStudent = stateStudent.get;
 
-  const searchData = () => {
-    const result = schoolLevel?.listOfAllStudents?.filter(
-      (student: any) => student.studentid === selectedStudent,
-    );
+  const schoolid = 1;
+  const midasStore = useMidasStore();
 
-    if (result?.length > 0) return result[0];
-
-    return '';
-  };
-
-  const computeConfidenceForEachStudent = (data: any) => {
-    let confidence: number = 0;
-    if (data) {
-      confidence = Math.round(
-        (data?.math_confidence +
-          data?.susp_confidence +
-          data?.read_confidence +
-          data?.odr_confidence) /
-          4,
-      );
-    }
-    return confidence;
-  };
-
-  const data: any = searchData();
-
-  let confidence: number = computeConfidenceForEachStudent(data);
-
-  const [saebrsScores, setSaebrsScores] = useState({
-    saebrsTotal: 'NA',
-    mySaebrsTotal: 'NA',
-    saebrsEmotional: 'NA',
-    mySaebrsEmotional: 'NA',
-    saebrsSocial: 'NA',
-    mySaebrsSocial: 'NA',
-    saebrsAcademic: 'NA',
-    mySaebrsAcademic: 'NA',
-  });
-
-  const [midasSummary, setMidasSummary] = useState({
-    midasRisk: 'NA',
-    missingVariables: 1,
-    confidence: 0,
-    confidenceThreshold: [1, 2, 3, 4, 5],
-  });
-
-  const [identification, setIdentification] = useState({
-    studentId: data?.studentid ?? 'NA',
-    grade: data?.gradelevel ?? 'NA',
-    classroomId: data?.classroom ?? 'NA',
-  });
-
-  const [demographics, setDemographics] = useState({
-    gender: data?.gender ?? 'NA',
-    ethnicity: data?.ethnicity ?? 'NA',
-    englishLearner: data?.ell ?? 'NA',
-  });
-
-  const [testScoreRisk, setTestScoreRisk] = useState({
-    math: data?.math_risk ?? 'NA',
-    reading: data?.reading_risk ?? 'NA',
-    suspension: 'NA',
-    odr: 'NA',
-  });
+  const [schoolData, setSchoolData] = useState<SchoolData[]>([]);
 
   useEffect(() => {
-    setDemographics({
-      gender: data?.gender ?? 'NA',
-      ethnicity: data?.ethnicity ?? 'NA',
-      englishLearner: data?.ell ?? 'NA',
-    });
+    const school = midasStore.getStudentsByGradeLevel(schoolid, 7);
+    console.log("Student data:", school);
 
-    setIdentification({
-      studentId: data?.studentid ?? 'NA',
-      grade: data?.gradelevel ?? 'NA',
-      classroomId: data?.classroom ?? 'NA',
-    });
+    setSchoolData(school);
+  }, [midasStore, schoolid]);
 
-    setSaebrsScores({
-      saebrsTotal: 'NA',
-      mySaebrsTotal: 'NA',
-      saebrsEmotional: data?.saebrs_emo,
-      mySaebrsEmotional: data?.mysaebrs_emo,
-      saebrsSocial: data?.saebrs_soc,
-      mySaebrsSocial: data?.mysaebrs_soc,
-      saebrsAcademic: data?.saebrs_aca,
-      mySaebrsAcademic: data?.mysaebrs_aca,
-    });
+  const dashboardData: DashboardData = {
+    midasRiskPercentages: calculateRiskPercentages(schoolData!, 'midas'),
+    teacherRiskPercentages: calculateRiskPercentages(schoolData!, 'teacher'),
+    studentRiskPercentages: calculateRiskPercentages(schoolData!, 'student'),
 
-    setTestScoreRisk({
-      math: data?.math_risk ?? 'NONE',
-      reading: data?.read_risk ?? 'NONE',
-      suspension: data?.susp_risk ?? 'NONE',
-      odr: data?.odr_risk ?? 'NONE',
-    });
+    midasConfidence: calculateModeConfidence(schoolData!, 'midas'), // example value
 
-    setMidasSummary({
-      midasRisk: 'NA',
-      missingVariables: 1,
-      confidence:
-        Math.round(
-          (data?.math_confidence +
-            data?.read_confidence +
-            data?.susp_confidence) /
-            3,
-        ) ?? 0,
-      confidenceThreshold: [1, 2, 3, 4, 5],
-    });
-  }, [data]);
+    odrPercentages: calculateOccurancePercentages(schoolData!, 'odr_f'),
+    suspPercentages: calculateOccurancePercentages(schoolData!, 'susp_f'),
+
+    mathPercentages: calculateTestRiskPercentages(schoolData!, 'math_f'),
+    readPercentages: calculateTestRiskPercentages(schoolData!, 'read_f'),
+
+    ethnicityRiskPercentages: {
+      white: calculateRiskByDemographic(schoolData!, 'midas', 'ethnicity', 'White'),
+      hispanic: calculateRiskByDemographic(schoolData!, 'midas', 'ethnicity', 'Hispanic'),
+      other: calculateRiskByDemographic(schoolData!, 'midas', 'ethnicity', 'Other POC'),
+    },
+
+    ellRiskPercentages: {
+      ell: calculateRiskByDemographic(schoolData!, 'midas', 'ell', 'Yes'),
+      nonEll: calculateRiskByDemographic(schoolData!, 'midas', 'ell', 'No'),
+    },
+    genderRiskPercentages: {
+      male: calculateRiskByDemographic(schoolData!, 'midas', 'gender', 'Male'),
+      female: calculateRiskByDemographic(schoolData!, 'midas', 'gender', 'Female'),
+    },
+  };
 
   //handle export feature
-  const handleExport = async (listStudents: any) => {
-    const result = await postData({
-      url: 'https://midas-topaz.vercel.app/api/export',
-      data: { listStudents },
-    });
-    const res = writeFile(result, 'students.xlsx', {
-      compression: true,
-      type: 'file',
-    });
-  };
+  //const handleExport = async (listStudents: any) => {
+  //  const result = await postData({
+  //    url: 'https://midas-topaz.vercel.app/api/export',
+  //    data: { listStudents },
+  //  });
+  //  const res = writeFile(result, 'students.xlsx', {
+  //    compression: true,
+  //    type: 'file',
+  //  });
+  //};
   return (
-    <main className='flex flex-col gap-2 m-2'>
-      <div className='flex flex-col lg:flex-row gap-1'>
-        <StudentSearch
-          selectedStudent={selectedStudent}
-          setSelectedStudent={setSelectedStudent}
-          data={data}
-          className='lg:basis-1/3'
-        />
+    <main className="grid grid-cols-2 grid-rows-5 max-w-[50%] mx-auto">
 
-        <div className='flex lg:hidden flex-row gap-1'>
-          <CardMidasRisk 
-            midasRisk={midasSummary.midasRisk} 
-            className='basis-1/2'
-          />
-          <CardConfidenceVisualizer
-            confidence={3}
-            confidenceThresholds={[1, 2, 3, 4, 5]}
-            missingVariables={0}
-            className='basis-1/2 '
-          />
-        </div>
+      <RiskCard
+        title={'MIDAS Main Risk'}
+        assessments={[
+          {
+            name: '',
+            values: [dashboardData.midasRiskPercentages.low, dashboardData.midasRiskPercentages.some, dashboardData.midasRiskPercentages.high],
+            labels: ['Low', 'Some', 'High'],
+            tooltipContent: MidasRiskScoreTooltip()
+          },
+        ]}
+        className=""
+      />
+      <CardConfidenceVisualizer
+        missingVariables={0}
+        confidence={dashboardData.midasConfidence}
+        confidenceThresholds={[1, 2, 3, 4, 5]}
+        className=""
+      />
+      <RiskCard
+        title={'Teacher Sub-Risk'}
+        assessments={[
+          {
+            name: '',
+            values: [dashboardData.studentRiskPercentages.low, dashboardData.studentRiskPercentages.some, dashboardData.studentRiskPercentages.high],
+            labels: ['Low', 'Some', 'High'],
+            tooltipContent: 'Sub risk'
+          },
+        ]}
+        className=''
+      />
+      <RiskCard
+        title={'Student Sub-Risk'}
+        assessments={[
+          {
+            name: '',
+            values: [dashboardData.teacherRiskPercentages.low, dashboardData.teacherRiskPercentages.some, dashboardData.teacherRiskPercentages.high],
+            labels: ['Low', 'Some', 'High'],
+            tooltipContent: 'Sub risk'
+          },
+        ]}
+        className=''
+      />
 
-        <div className='flex flex-col lg:flex-row gap-1 lg:basis-2/3'>
-          <CardStudentDiscipline
-            odr={testScoreRisk.odr}
-            suspensions={testScoreRisk.suspension}
-          />
-          <CardStudentTestScores
-            math={testScoreRisk.math}
-            reading={testScoreRisk.reading}
-          />
-        </div>
-      </div>
-      
-      
+      {/* Row 2 */}
+      <RiskCard
+        title={'Discipline Summary'}
+        assessments={[
+          {
+            name: 'ODR',
+            values: [dashboardData.odrPercentages.zero, dashboardData.odrPercentages.oneplus],
+            labels: ['Zero', 'One +'],
+            tooltipContent: 'ODR'
+          },
+          {
+            name: 'Suspensions',
+            values: [dashboardData.suspPercentages.zero, dashboardData.suspPercentages.oneplus],
+            labels: ['Zero', 'One +'],
+            tooltipContent: 'Suspensions'
+          }
+        ]}
+        className=""
+      />
 
-      <div className='flex flex-row gap-1 h-[29vh]'>
-        <div className='hidden lg:flex flex-col gap-1 basis-1/4 '>
-          <CardMidasRisk 
-            midasRisk={midasSummary.midasRisk} 
-            className='h-full'
-          />
-          <CardConfidenceVisualizer
-            confidence={3}
-            confidenceThresholds={[1, 2, 3, 4, 5]}
-            missingVariables={0}
-            className=''
-          />
-        </div>
+      <RiskCard
+        title={'Test Risk Scores'}
+        assessments={[
+          {
+            name: 'Math',
+            values: [dashboardData.mathPercentages.low, dashboardData.mathPercentages.some, dashboardData.mathPercentages.high],
+            labels: ['Low', 'Some', 'High'],
+            tooltipContent: 'ODR'
+          },
+          {
+            name: 'Reading',
+            values: [dashboardData.mathPercentages.low, dashboardData.mathPercentages.some, dashboardData.mathPercentages.high],
+            labels: ['Low', 'Some', 'High'],
+            tooltipContent: ''
+          }
+        ]}
+        className=""
+      />
 
-        <div className='lg:basis-3/4 w-full'>
-          <SaebrsSummary
-            saebrsTotal={saebrsScores.saebrsTotal}
-            mySaebrsTotal={saebrsScores.mySaebrsTotal}
-            saebrsEmotional={saebrsScores.saebrsEmotional}
-            mySaebrsEmotional={saebrsScores.mySaebrsEmotional}
-            saebrsSocial={saebrsScores.saebrsSocial}
-            mySaebrsSocial={saebrsScores.mySaebrsSocial}
-            saebrsAcademic={saebrsScores.saebrsAcademic}
-            mySaebrsAcademic={saebrsScores.mySaebrsAcademic}
-            className='gap-1 h-full'
-            className_card='min-h-[28vh]'
-          />
-        </div>
-        
-      </div>
-
-      <div className="invisible lg:visible z-10 absolute bottom-10 right-28 opacity-75">
-        <Button className="bg-[#1e8434] hover:bg-[#1e8434a1]" onClick={() => handleExport(schoolLevel?.listOfAllStudents)}>
-          <ArrowDownTrayIcon className="w-6 pr-2" />
-          <p>Export</p>
-        </Button>
-      </div>
     </main>
+
   );
 }
