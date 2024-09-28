4488821710fa6a3e6ce5e04e6c538f9c2dab9eeb
Gabriel Hooks <gabriel.i.hooks@gmail.com>
2024-09-28 19:46:09 -0400

    Removed unused lib/ directory and its contents

--- app/dashboard/classroom/page.tsx
+++ app/dashboard/classroom/page.tsx
@@ -12,6 +12,7 @@ import useMidasStore, { SchoolData } from '@/hooks/useSchoolData';
 import { calculateModeConfidence, calculateOccurancePercentages, calculateRiskByDemographic, calculateRiskPercentages, calculateTestRiskPercentages } from '@/action/calculateRiskStatistics';
 import ClassSearch from '@/app/ui/dashboard/cards/search/class-search-card';
 import { GetClassroomOptions } from '@/action/getClassroomOptions';
+import { RiskCardWithConfidence } from '@/app/ui/dashboard/risk-confidence-card';
 
 
 export default function Page() {
@@ -60,9 +61,15 @@ export default function Page() {
   };
   return (
     <main className='lg:max-h-[90vh] grid max-md:grid-cols-1 max-md:grid-rows-none max-lg:grid-cols-2 lg:grid-cols-4 max-lg:grid-rows-1 lg:grid-rows-6 gap-4'>
-      <ClassSearch selectedClass={selectedClass} setSelectedClass={setSelectedClass} classList={GetClassroomOptions(schoolData)} studentList={schoolData.map(student => student.studentid)} />
+      <ClassSearch
+        selectedClass={selectedClass}
+        setSelectedClass={setSelectedClass}
+        classList={GetClassroomOptions(schoolData)}
+        studentList={schoolData.map(student => student.studentid)}
+      />
+
       {/* Row 1 */}
-      <RiskCard
+      <RiskCardWithConfidence
         title={'MIDAS Main Risk'}
         assessments={[
           {
